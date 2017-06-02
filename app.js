f147cdf6ef8d3a25ef616aad9ffcf79b08b22b47
Liuhuaying Yang <lyang@mbta.com>
2017-06-02 09:51:37 -0400

    draw stops and shapes based on GTFS data

--- app.js
+++ app.js
@@ -1,77 +1,106 @@
 //global url
-stationurl = 'https://api.mbtace.com/stops?route='
+var	stopsUrl = '/data/stops.txt',
+	shapesUrl = '/data/shapes.txt',
+	tripsUrl = '/data/trips.txt',
+	routesUrl = '/data/routes.txt',
+	stoptimesUrl = '/data/stop_times.txt';
 // Define the `spApp` module
 var app = angular.module('spApp', []);
 
 // Define the `spAppController` controller on the `spApp` module
-app.controller('spAppController', function spAppController($scope,$http) {
-  
-  var map = L.map('mapid')
-  map.setView(new L.LatLng(42.351486,-71.066829), 13);
-  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(map);
-  map.zoomControl.setPosition('topright')
-  // possible tile values 
-  // light_all,
-  // dark_all,
-  // light_nolabels,
-  // light_only_labels,
-  // dark_nolabels,
-  // dark_only_labels
-  // $scope.stations = [];
-$scope.getStops = function(route,stoptype){  
-  $http.get(stationurl + route)
-    .then(function(response) {
-      console.log(response.data.data)
-      //get the data
-      $scope.stations = response.data.data
-      //set the latlng collection for route, set the flag for selected status
-      var latlngs = [], isClicked = false;
-      //create a popup
-      var popup = L.popup({className:'popup',closeButton:false})
-      //draw station/stop and route
-      angular.forEach($scope.stations,function(station){
+app.controller('spAppController', function spAppController($scope,$http,$q) {
+	var map = L.map('mapid');
+	map.setView(new L.LatLng(42.351486,-71.066829), 15);
+	L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(map);
+	  // possible tile values 
+	  // light_all,
+	  // dark_all,
+	  // light_nolabels,
+	  // light_only_labels,
+	  // dark_nolabels,
+	  // dark_only_labels
+	map.zoomControl.setPosition('topright')
 
-        //create a marker for station/stop
-        L.marker([station.attributes.latitude,station.attributes.longitude], {icon:stoptype == 'station'?stationImg:stopImg})
-          .on('mouseover',function(){
-            //set location and content for station/stop popup
-            popup.setLatLng([station.attributes.latitude,station.attributes.longitude])
-              .setContent(station.attributes.name)
-              .openOn(map);
-          })
-          .on('mouseout',function(){
-            map.closePopup()
-          })
-          .on('click',function(){
-            console.log(this.options.icon)
-            // this.options.icon.options.iconUrl = 'images/bus-selected.png'
-            // this._icon.src = 'images/bus-selected.png'
-          })
-          .addTo(map);
-        //populate the latlngs
-        latlngs.push([station.attributes.latitude,station.attributes.longitude])
-      })
-      //draw route
-      var polyline = L.polyline(latlngs, {className: 'path'}).addTo(map)
-        .on('mouseover',function(e){
-          this._path.classList.add('selected');
-          console.log(e)
-          //set location and content for station/stop popup
-          popup.setLatLng([e.latlng.lat,e.latlng.lng])
-            .setContent(route)
-            .openOn(map);
-        })
-        .on('mouseout',function(){
-          map.closePopup()
-          if(!isClicked){this._path.classList.remove('selected')}})
-        .on('click',function(){
-          isClicked = isClicked ? false : true;
-          if(isClicked){this._path.classList.add('selected')}
-          else{this._path.classList.remove('selected')}
-        });
-    });
-}
-$scope.getStops('Green-E','station')
-$scope.getStops('Green-D','stop')
+	//create a popup
+    var popup = L.popup({className:'popup',closeButton:false})
+
+
+
+	$scope.drawStops = function(data){
+		data.forEach(function(stop){
+			// L.circleMarker([stop.stop_lat,stop.stop_lon], {radius:10}).addTo(map);
+			L.circle([stop.stop_lat,stop.stop_lon], {radius:6,className:'stop'})
+			.on('mouseover',function(){
+	            //set location and content for station/stop popup
+	            popup.setLatLng([stop.stop_lat,stop.stop_lon])
+	              .setContent(stop.stop_name)
+	              .openOn(map);
+	          })
+	          .on('mouseout',function(){
+	            map.closePopup()
+	          })
+			.addTo(map);
+
+		})
+	}
+	$scope.drawShapes = function(data){
+		//nest by shape id
+		const shapesById = d3.nest()
+			.key(function(d){return d.shape_id})
+			.sortValues(function(a,b) { return +b.shape_pt_sequence - +a.shape_pt_sequence; })
+			.entries(data)
+		console.log(shapesById)
+		shapesById.forEach(function(shape){
+			var shapepts = []
+			shape.values.forEach(function(shapept){
+				shapepts.push([shapept.shape_pt_lat,shapept.shape_pt_lon])
+			})
+			L.polyline(shapepts, {className: 'path'})
+			.on('mouseover',function(e){
+	        	this._path.classList.add('selected');
+	          //set location and content for station/stop popup
+	        	popup.setLatLng([e.latlng.lat,e.latlng.lng])
+		            .setContent(shape.key)
+		            .openOn(map);
+	        })
+	        .on('mouseout',function(){
+	        	this._path.classList.remove('selected');
+	         	map.closePopup();
+	        })
+			.addTo(map);
+		})
+	}
+
+	$q.all([$http.get(stopsUrl),$http.get(shapesUrl),$http.get(tripsUrl),$http.get(routesUrl),$http.get(stoptimesUrl)]).then(function(allData){ 
+		// console.log(allData[0].data)
+		$scope.stops = parseData(allData[0].data)
+		$scope.shapes = parseData(allData[1].data)
+		$scope.trips = parseData(allData[2].data)
+		$scope.routes = parseData(allData[3].data)
+		$scope.stoptimes = parseData(allData[4].data)
+		
+		$scope.drawShapes($scope.shapes)
+		$scope.drawStops($scope.stops)
+	});
+
 });
+
+function parseData(data){
+	const newData = [];
+	const rows = data.split(/\r\n|\n/);
+	const title = rows[0].split(',').map(function(d){return d.split('"').join('')});
+	rows.forEach(function(row,i){
+		let newRow = {};
+		let rowsplit = row.split(',').map(function(d){return d.split('"').join('')});
+		if(i > 0){
+			title.forEach(function(col,n){
+
+				newRow[col] = rowsplit[n]
+				
+			})
+			newData.push(newRow)
+		}		
+	})
+	return newData
+}
 
